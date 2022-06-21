9ed9fb9f6e9d4dcb56fdabc7b3977a65f7d26f20
jacklin <jacklin@ekoral.net>
2022-06-21 20:51:27 +0800

    web-dev-server.config

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
-import {html} from "lit";
+import { html } from "lit";
 
 export const PREVIOUS = html`
 <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.1.1/css/all.css">
